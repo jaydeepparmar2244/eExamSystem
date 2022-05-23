bcec5b74362c172ec123bef7c7e45ae243e13778
jaydeepparmar2244 <jaydeep2244.67@gmail.com>
2022-05-23 14:27:26 +0530

    Questions push to exam error solved!

--- controller/session-controller.js
+++ controller/session-controller.js
@@ -28,22 +28,22 @@ function saveuser(req, res) {
 
 module.exports.mailLinkToResetPassword = function (req, res) {
     let email = req.body.email
-    if(email===null){
+    if (email === null) {
         // console.log('email requireed')
-        res.json({msg:"Email is required!",status:-1,data:err})
+        res.json({ msg: "Email is required!", status: -1, data: err })
     }
-    else{
-        UserModel.findOne({email:email},function(err,data){
-            if(err || data ==null){
+    else {
+        UserModel.findOne({ email: email }, function (err, data) {
+            if (err || data == null) {
                 // console.log('email not found!')
-                res.json({msg:"Email not found",status:-1,data:err})
+                res.json({ msg: "Email not found", status: -1, data: err })
             }
-            else{
+            else {
                 const token = crypto.randomBytes(20).toString('hex');
                 // console.log(token)
                 data.update({
-                    resetPasswordToken:token,
-                    resetPasswordExpires:Date.now() + 3600000
+                    resetPasswordToken: token,
+                    resetPasswordExpires: Date.now() + 3600000
                 });
                 var transporter = nodemailer.createTransport({
                     service: 'gmail',
@@ -58,40 +58,41 @@ module.exports.mailLinkToResetPassword = function (req, res) {
                     from: process.env.EMAIL_ID,
                     to: data.email,
                     subject: 'Link to reset Password!',
-                    text: "click on this link to reset your password:"+`http://localhost:3000/reset/${token}`
+                    text: "click on this link to reset your password:" + `http://localhost:3000/reset/${token}`
                 };
                 transporter.sendMail(mailOptions, function (err, data) {
                     if (err) {
                         console.log(err);
                     } else {
                         console.log('Email sent: ' + data.response);
-                        res.json({msg:"Email Sent!",status:200,data:data})
+                        res.json({ msg: "Email Sent!", status: 200, data: data })
                     }
                 });
             }
         })
 
     }
-   
+
 }
 
-module.exports.resetPassword = function(req,res){
+module.exports.resetPassword = function (req, res) {
     let email = req.body.email
-    let password =req.body.password
-    let hashPassword = bcrypt.hashSync(password,10)
-    UserModel.findOneAndUpdate({email:email},{password:hashPassword},function(err,data){
-        if(err){
-            res.json({msg:"Error",status:-1,data:err})
+    let password = req.body.password
+    let hashPassword = bcrypt.hashSync(password, 10)
+    UserModel.findOneAndUpdate({ email: email }, { password: hashPassword }, function (err, data) {
+        if (err) {
+            res.json({ msg: "Error", status: -1, data: err })
         }
-        else if(data==null){
-            res.json({msg:"Email Not FOund!",status:-1,data:data})
+        else if (data == null) {
+            res.json({ msg: "Email Not FOund!", status: -1, data: data })
         }
-        else{
-            res.json({msg:"Password Reset!",status:200,data:data})
+        else {
+            res.json({ msg: "Password Reset!", status: 200, data: data })
         }
     })
-    
+
 }
+
 //exporting
 module.exports.saveuser = saveuser
 module.exports.login = login
